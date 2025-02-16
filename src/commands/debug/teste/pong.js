5c445de06c281510754715acbd080d9a036ad671
Geovane Saraiva da Silva <geovane.saraiva.silva75@gmail.com>
2025-02-16 19:31:49 -0300

    Docs: Adicionar JSDocs para os comandos Ping e Pong

--- src/commands/debug/teste/pong.js
+++ src/commands/debug/teste/pong.js
@@ -1,6 +1,14 @@
+import { CommandInteraction } from "discord.js";
 import { Command } from "../../../core/Command";
 
+/**
+ * Comando que responde com "Ping!".
+ * @extends {Command}
+ */
 export default class PongCommand extends Command {
+    /**
+     * Cria uma nova instância do comando Pong.
+     */
     constructor() {
         super();
         this.setName("pong");
@@ -10,6 +18,11 @@ export default class PongCommand extends Command {
         this.setDisabled(false);
     }
 
+    /**
+     * Executa o comando Pong.
+     * @param {DiscordClient} client - A instância do cliente customizado do Discord.
+     * @param {CommandInteraction} interaction - A interação que acionou o comando.
+     */
     execute(client, interaction) {
         interaction.reply("Ping!");
     }
--- src/commands/guild/teste/ping.js
+++ src/commands/guild/teste/ping.js
@@ -1,6 +1,14 @@
+import { CommandInteraction } from "discord.js";
 import { Command } from "../../../core/Command";
 
+/**
+ * Comando que responde com "Pong!".
+ * @extends {Command}
+ */
 export default class PingCommand extends Command {
+    /**
+     * Cria uma nova instância do comando Ping.
+     */
     constructor() {
         super();
         this.setName("ping");
@@ -9,6 +17,11 @@ export default class PingCommand extends Command {
         this.setDisabled(false);
     }
 
+    /**
+     * Executa o comando Ping.
+     * @param {DiscordClient} client - A instância do cliente customizado do Discord.
+     * @param {CommandInteraction} interaction - A interação que acionou o comando.
+     */
     execute(client, interaction) {
         interaction.reply("Pong!");
     }
