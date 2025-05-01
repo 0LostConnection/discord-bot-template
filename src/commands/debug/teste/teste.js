fed2fcf2b7d4a8d2a943e1a2280cf4fdedcd269f
Geovane Saraiva da Silva <geovane.saraiva.silva75@gmail.com>
2025-05-01 09:45:21 -0300

    Update teste.js

--- src/commands/debug/teste/teste.js
+++ src/commands/debug/teste/teste.js
@@ -1,8 +1,11 @@
 import { CommandInteraction } from "discord.js";
-
 import { DiscordClient } from "@core/DiscordClient";
 import { Command } from "@core/Command";
-import t_ContainerBuilder from "@builders/ContainerBuilder";
+import {
+    ContainerBuilder,
+    SectionBuilder,
+    TextDisplayBuilder,
+} from "@builders";
 
 /**
  * Comando de teste do builder de container
@@ -22,13 +25,22 @@ export default class Teste extends Command {
      * @param {CommandInteraction} interaction - A interação que acionou o comando.
      */
     execute(client, interaction) {
-        const container = new t_ContainerBuilder();
-
-        container.setColor("#34d2eb").addTextDisplayComponent("Teste");
+        const embedV2 = new ContainerBuilder()
+            .setColor("#921AC5")
+            .addComponent([
+                new TextDisplayBuilder("# Teste\n## Teste\n### Teste"),
+                new SectionBuilder()
+                    .addTextDisplay(`${interaction.user}`)
+                    .addThumbnail({
+                        url: interaction.user.displayAvatarURL(),
+                        description: `Foto de ${interaction.user.username}`,
+                        spoiler: true,
+                    }),
+            ]);
 
         interaction.reply({
             flags: ["IsComponentsV2", "Ephemeral"],
-            components: [container.build()],
+            components: [embedV2],
         });
     }
 }
