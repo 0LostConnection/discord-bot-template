990105a12b3ab4ea534473a75fe9f3a7121fe9f7
Geovane Saraiva da Silva <geovane.saraiva.silva75@gmail.com>
2025-05-01 13:18:08 -0300

    Update teste.js

--- src/commands/debug/teste/teste.js
+++ src/commands/debug/teste/teste.js
@@ -1,10 +1,20 @@
-import { CommandInteraction } from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    CommandInteraction,
+    MediaGalleryBuilder,
+    MediaGalleryItemBuilder,
+    SeparatorBuilder,
+    SeparatorSpacingSize,
+    TextDisplayBuilder,
+} from "discord.js";
 import { DiscordClient } from "@core/DiscordClient";
 import { Command } from "@core/Command";
 import {
-    ContainerBuilder,
-    SectionBuilder,
-    TextDisplayBuilder,
+    ContainerBuilder_,
+    SectionBuilder_,
+    TextDisplayBuilder_,
 } from "@builders";
 
 /**
@@ -25,22 +35,55 @@ export default class Teste extends Command {
      * @param {CommandInteraction} interaction - A interação que acionou o comando.
      */
     execute(client, interaction) {
-        const embedV2 = new ContainerBuilder()
+        const componentsArray = [
+            new TextDisplayBuilder_("# Teste\n## Teste\n### Teste"),
+            new SectionBuilder_()
+                .addTextDisplay(`${interaction.user}\nTeste\nTeste\nTeste00`)
+                .addTextDisplay(`Teste 1`)
+                .addTextDisplay(`Teste 2`)
+                .addThumbnail({
+                    url: interaction.user.displayAvatarURL(),
+                    description: `Foto de ${interaction.user.username}`,
+                    spoiler: true,
+                }),
+            new SectionBuilder_()
+                .addTextDisplay(`${interaction.user}\nTeste\nTeste\nTeste00`)
+                .addTextDisplay(new TextDisplayBuilder_("teste1"))
+                .addTextDisplay(new TextDisplayBuilder().setContent("Teste2"))
+                .setAccessory(
+                    new ButtonBuilder()
+                        .setStyle(ButtonStyle.Link)
+                        .setEmoji("✴")
+                        .setLabel("Imagem do Usuário")
+                        .setURL(
+                            interaction.user.displayAvatarURL({
+                                extension: "png",
+                                size: 1024,
+                            }),
+                        ),
+                ),
+
+            new MediaGalleryBuilder().addItems(
+                new MediaGalleryItemBuilder()
+                    .setURL(
+                        "https://previews.123rf.com/images/imagecatalogue/imagecatalogue1611/imagecatalogue161116759/66637610-test-text-rubber-seal-stamp-watermark-tag-inside-rounded-rectangular-banner-with-grunge-design-and.jpg",
+                    )
+                    .setDescription("Teste"),
+            ),
+            new TextDisplayBuilder_("### Lorem ipsum dolor sit amet"),
+            new SeparatorBuilder({
+                divider: true,
+                spacing: SeparatorSpacingSize.Large,
+            }),
+        ];
+
+        const embedV2 = new ContainerBuilder_()
             .setColor("#921AC5")
-            .addComponent([
-                new TextDisplayBuilder("# Teste\n## Teste\n### Teste"),
-                new SectionBuilder()
-                    .addTextDisplay(`${interaction.user}`)
-                    .addThumbnail({
-                        url: interaction.user.displayAvatarURL(),
-                        description: `Foto de ${interaction.user.username}`,
-                        spoiler: true,
-                    }),
-            ]);
+            .addComponents(componentsArray);
 
         interaction.reply({
             flags: ["IsComponentsV2", "Ephemeral"],
-            components: [embedV2],
+            components: [embedV2, ...componentsArray],
         });
     }
 }
