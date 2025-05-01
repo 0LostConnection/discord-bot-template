5fd0538fb750c2ee6c7deae75dd25a3e5ce1801a
Geovane Saraiva da Silva <geovane.saraiva.silva75@gmail.com>
2025-05-01 09:47:15 -0300

    feat: barrel exports

---
+++ src/builders/index.js
@@ -0,0 +1,3 @@
+export { default as ContainerBuilder } from './ContainerBuilder';
+export { default as TextDisplayBuilder } from './TextDisplayBuilder';
+export { default as SectionBuilder } from './SectionBuilder';
